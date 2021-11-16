d602b901be1e33f37d133fb9f90d80046d2d99f1
Asker Brejnrod <askerbrejnrod@Askers-MacBook-Pro-2.local>
2021-11-16 11:00:45 +0100

    added extra columns to results for rules

--- pici_finder.py
+++ pici_finder.py
@@ -3,6 +3,9 @@ import subprocess
 import pandas as pd
 import re
 import os
+import sys
+
+
 def run_tblastn(dbpath, inputfile, outfile, evalue = "0.001", cpus = 1):
      out = subprocess.run(["tblastn", "-query", dbpath, "-subject", inputfile, "-out", outfile,
                            "-evalue", evalue, "-outfmt", "6", "-num_threads", str(cpus)])
@@ -16,33 +19,48 @@ def is_colocal(df, gene_class, int_sstart, trim_threshold):
         return(False)
     return(ret)
 
-def pri_rep_location(df):
-    df_gene = df[df["Gene_class"] == "pri-rep"]
+def gene_best_hit_location(df, gene):
+    df_gene = df[df["Gene_class"] == gene]
     try:
         ret = df_gene['sstart'][df_gene['evalue'].idxmin()]
     except:
         return(0)
     return(ret)
 
+def pici_direction(int_location, alpA_location):
+     if alpA_location > int_location:
+          return(1)
+     else:
+          return(-1)
+     
 def search(x):
     df = tblastn_results[tblastn_results['sseqid'] == x['sseqid']]
-    result = {'Integrase': x['qseqid'], 'Integrase_pident': x['pident'], 'alpA': is_colocal(df, "alpA", x['sstart'], trim_threshold),
+    direction = pici_direction(x['sstart'], gene_best_hit_location(df, "alpA"))
+    result = {'Integrase': x['qseqid'], 'Integrase_pident': x['pident'],
+              'int_location': x['sstart'],
+              'alpA': is_colocal(df, "alpA", x['sstart'], trim_threshold),
+              'alpA_location' : gene_best_hit_location(df, "alpA"), 
               'prirep': is_colocal(df, "pri-rep", x['sstart'], trim_threshold),
+              'prirep_location': gene_best_hit_location(df, "pri-rep"),
               'sis': is_colocal(df, 'sis', x['sstart'], trim_threshold),
               'Contig': x['sseqid'], "Genome": x["Genome_name"],
-              "PICI_start": pri_rep_location(df) - pri_rep_trim, "PICI_end": pri_rep_location(df) + pri_rep_trim}
+              'direction': pici_direction(x['sstart'], gene_best_hit_location(df, "alpA")),
+              "PICI_start": gene_best_hit_location(df, "pri-rep") - direction*attr_trim, "PICI_end": gene_best_hit_location(df, "pri-rep") + direction*attl_trim}
     return(result)
 
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
-#r = run_tblastn("databases/BLAST_protein_db.faa", "data/Alfred/GCA_000025745.1_ASM2574v1_genomic.fna", "tblastn.tsv")
-int_threshold = 70
+int_threshold = 40
+pri_rep_ident_threshold = 50
+
 trim_threshold = 30000
 pri_rep_trim = 25000
+attr_trim = 2000
+attl_trim = 25000
 genome_path = "data/Alfred/"
 force = True
-output_dir = "slet_sensitivity"
+output_dir = "slet_virome_sensitivity"
 cpus = 4
 try:
     os.mkdir("output_dir")
@@ -52,8 +70,11 @@ except FileExistsError:
     print("Directory already exists")        
 
 result_list = []
-for genome in os.listdir(genome_path)[1:5]:
-    print(genome)
+for index, genome in enumerate(os.listdir(genome_path)[0:3]):
+    perc = index/len(os.listdir(genome_path))*100
+    if perc % 1 == 0:
+        print( "\r" + str(perc) )
+        sys.stdout.flush()
     r = run_tblastn("databases/BLAST_protein_db.faa", genome_path+genome, "tblastn.tsv", cpus = cpus)
     tblastn_result = pd.read_csv("tblastn.tsv", sep = "\t",  names=['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend',
                                                                 'sstart', 'send', 'evalue', 'bitscore'])
@@ -78,7 +99,7 @@ tblastn_results = pd.concat(result_list)
 
 
 pici_results =  tblastn_results[tblastn_results['int_match'] == True].apply(search, axis = 1, result_type = 'expand')
-print(pici_results)
+#print(pici_results)
 
     
 
